a850509583028914d7c89e32e72dc6f686e55ec8
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2026-03-14 13:19:08 +0100

    setting up the playground project

---
+++ src/main/java/com/lith/playground/section01/SectionRunner.java
@@ -0,0 +1,17 @@
+package com.lith.playground.section01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SectionRunner.class,
+                "--section=section01",
+                "--config=03-consumer-group"
+        );
+    }
+
+}
--- src/test/java/com/lith/even_driven_playground/TestEvenDrivenPlaygroundApplication.java
+++ src/test/java/com/lith/even_driven_playground/TestEvenDrivenPlaygroundApplication.java
@@ -1,11 +1,12 @@
 package com.lith.even_driven_playground;
 
+import com.lith.playground.section01.SectionRunner;
 import org.springframework.boot.SpringApplication;
 
 public class TestEvenDrivenPlaygroundApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(EvenDrivenPlaygroundApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(SectionRunner::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
