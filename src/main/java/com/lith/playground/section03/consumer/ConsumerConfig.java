4ba53fd226b63aa3b3c2ef9e95c909e452937d15
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2026-03-15 19:29:09 +0100

    produce messages with keys

--- src/main/java/com/lith/playground/section03/consumer/ConsumerConfig.java
+++ src/main/java/com/lith/playground/section03/consumer/ConsumerConfig.java
@@ -3,6 +3,8 @@ package com.lith.playground.section03.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 
@@ -13,5 +15,17 @@ public class ConsumerConfig {
     @Bean
     public Consumer<String> consumer() {
         return msg -> log.info("received: {}", msg);
+    }
+
+    @Bean
+    public Consumer<Message<String>> messageConsumer() {
+        return this::handleMessage;
+    }
+
+    private void handleMessage(Message<String> message) {
+        log.info("message: {}", message);
+        Object key = message.getHeaders().get(KafkaHeaders.RECEIVED_KEY);
+        Object payload = message.getPayload();
+        log.info("key: {}, payload: {}", key, payload);
     }
 }
--- src/main/java/com/lith/playground/section03/producer/ProducerConfig.java
+++ src/main/java/com/lith/playground/section03/producer/ProducerConfig.java
@@ -3,6 +3,9 @@ package com.lith.playground.section03.producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
@@ -19,5 +22,22 @@ public class ProducerConfig {
             log.info("produced: {}", msg);
             return msg;
         };
+    }
+
+    @Bean
+    public Supplier<Message<String>> messageProducer() {
+        AtomicInteger counter = new AtomicInteger(0);
+        return () -> {
+            Message<String> msg = this.buildMessage(counter.incrementAndGet());
+            log.info("produced: {}", msg);
+            return msg;
+        };
+    }
+
+    private Message<String> buildMessage(Integer input) {
+        return MessageBuilder.withPayload("msg-" + input)
+                .setHeader(KafkaHeaders.KEY, "key-" + input)
+                .setHeader("trace-id", "trace-" + input)
+                .build();
     }
 }
